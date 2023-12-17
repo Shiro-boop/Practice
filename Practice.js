380922bcf50dbea2362b37748df10087ed41612b
Alex <137304331+Shiro-boop@users.noreply.github.com>
2023-12-17 22:27:42 +0200

    Add files via upload

---
+++ Practice.js
@@ -0,0 +1,20 @@
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '')
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('Насколько вы оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('Насколько вы оцените его?', '');
+
+
+personalMovieDB.movies[a] = b
+personalMovieDB.movies[c] = d
+
+console.log(personalMovieDB);
